d858de419e92aef2bb3b53bc3721cf82086acd5a
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-09 15:28:02 -0400

    Updates for new cohort.

--- main.js
+++ main.js
@@ -1,71 +1,43 @@
-/*
-# ========================================================
-# = Initialization
-# ========================================================
-*/
+
+/**********
+ * DATA *
+ **********/
 
 const sixes = [];
 const doubleSixes = [];
 const twelves = [];
 const twenties = [];
 
-window.onload = init;
 
-function init() {
-    const d6Roll = document.querySelector('#d6-roll');
-    d6Roll.addEventListener('click', rollD6);
+/*************************
+ * RANDOM ROLL GENERATOR *
+ *************************/
 
-    const doubleD6Roll1 = document.querySelector('#double-d6-roll-1');
-    doubleD6Roll1.addEventListener('click', rollDoubleD6);
-    const doubleD6Roll2 = document.querySelector('#double-d6-roll-2');
-    doubleD6Roll2.addEventListener('click', rollDoubleD6);
 
-    const d12Roll = document.querySelector('#d12-roll');
-    d12Roll.addEventListener('click', rollD12);
+function getRandomNumber(max) {
+    const rand = Math.random();
+    const range = rand * max;
+    const result = Math.ceil(range);
     
-    const d20Roll = document.querySelector('#d20-roll');
-    d20Roll.addEventListener('click', rollD20);
-
-    const resetButton = document.querySelector('#reset-button')
-    resetButton.addEventListener('click', resetAllRolls);
-
-    setStartingImages();
+    return result;
 }
 
-function setStartingImages() {
-    const START_DIRECTORY = './images/start'
-    const SIX_SIDED_START_IMAGE = `${START_DIRECTORY}/d6.png`;
 
-    const d6Roll = document.querySelector('#d6-roll');
-    d6Roll.src = SIX_SIDED_START_IMAGE;
+/*******************
+ * YOUR CODE BELOW *
+ *******************/
 
-    const doubleD12Roll1 = document.querySelector('#double-d6-roll-1')
-    const doubleD12Roll2 = document.querySelector('#double-d6-roll-2');
-    doubleD12Roll1.src = SIX_SIDED_START_IMAGE;
-    doubleD12Roll2.src = SIX_SIDED_START_IMAGE;
 
-    const d12Roll = document.querySelector('#d12-roll');
-    d12Roll.src = `${START_DIRECTORY}/d12.jpeg`;
-    
-    const d20Roll = document.querySelector('#d20-roll');
-    d20Roll.src = `${START_DIRECTORY}/d20.jpg`;
-}
+/*******************
+ * EVENT LISTENERS *
+ *******************/
 
-/*
-# ========================================================
-# = Roll Functions
-# ========================================================
-*/
 
-/*
-# ========================================================
-# = Math Functions
-# ========================================================
-*/
+/****************
+ * MATH SECTION *
+ ****************/
 
-/*
-# ========================================================
-# = Helper Functions - Stretch Goals!
-# ========================================================
-*/
 
+/*********
+ * RESET *
+ *********/
