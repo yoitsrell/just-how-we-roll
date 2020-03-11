e28bb328184a373c1f2cc541591ad91a50b54644
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-11 05:28:23 -0400

    Initial

--- main.js
+++ main.js
@@ -35,17 +35,217 @@ const sortByNumber = function(arr) {
 /******************
  * BUTTON QUERIES *
  ******************/
-
-
+const dSix = document.querySelector("#d6-roll");
+const dSix1 = document.querySelector("#double-d6-roll-1")
+const dSix2 = document.querySelector("#double-d6-roll-2")
+const d12 = document.querySelector("#die-12")
+const dTwenty = document.querySelector('#d20-roll')
 /******************
  * CLICK HANDLERS *
  ******************/
+function sixGrab(){
+let number =  getRandomNumber(6) //ask why sixes.push(getRandomNumber(6)) didn't work
+sixes.push(number)
 
+switch (number){
+  case 1:
+    dSix.src = `images/d6/1.png`
+      break;
+  case 2:
+    dSix.src = `images/d6/2.png`
+      break;
+  case 3:
+    dSix.src = `images/d6/3.png`
+        break;
+    case 4:
+    dSix.src = `images/d6/4.png`
+    break;
+  case 5:
+    dSix.src = `images/d6/5.png` 
+      break
+    case 6:
+    dSix.src = `images/d6/6.png`
+      break;
+  default:
+    dSix.src = `images/start/d6/.png`
+        }
+}
+
+function doubleSixGrab(){
+  let num1 = getRandomNumber(6)
+  let num2 = getRandomNumber(6)
+  doubleSixes.push(num1 + num2)
+
+switch (num1){
+    case 1:
+      dSix1.src = `images/d6/1.png`
+        break;
+    case 2:
+      dSix1.src = `images/d6/2.png`
+        break;
+    case 3:
+      dSix1.src = `images/d6/3.png`
+          break;
+      case 4:
+      dSix1.src = `images/d6/4.png`
+      break;
+    case 5:
+      dSix1.src = `images/d6/5.png` 
+        break
+      case 6:
+      dSix1.src = `images/d6/6.png`
+        break;
+    default:
+      dSix1.src = `images/start/d6/.png`
+}
+
+switch (num2){
+            case 1:
+              dSix2.src = `images/d6/1.png`
+                break;
+            case 2:
+              dSix2.src = `images/d6/2.png`
+                break;
+            case 3:
+              dSix2.src = `images/d6/3.png`
+                  break;
+              case 4:
+              dSix2.src = `images/d6/4.png`
+              break;
+            case 5:
+              dSix2.src = `images/d6/5.png` 
+                break
+              case 6:
+              dSix2.src = `images/d6/6.png`
+                break;
+            default:
+              dSix2.src = `images/start/d6/.png`
+}
+}
+
+function twelveGrab(){
+  let num1 = getRandomNumber(12)
+  twelves.push(num1)
+
+  switch (num1){
+    case 1:
+      d12.src = `images/numbers/1.png`
+        break;
+    case 2:
+      d12.src = `images/numbers/2.png`
+        break;
+    case 3:
+      d12.src = `images/numbers/3.png`
+        break;
+    case 4:
+      d12.src = `images/numbers/4.png`
+        break;
+    case 5:
+      d12.src = `images/numbers/5.png` 
+        break
+    case 6:
+      d12.src = `images/numbers/6.png`
+        break;
+    case 7:
+      d12.src = `images/numbers/7.png`
+        break;
+    case 8:
+      d12.src = `images/numbers/8.png`
+        break;
+    case 9:
+      d12.src = `images/numbers/9.png`
+        break;
+    case 10:
+      d12.src = `images/numbers/10.png`
+        break;
+    case 11:
+      d12.src = `images/numbers/11.png`
+        break;
+    case 12:
+      d12.src = `images/numbers/12.png`
+    //default:
+      //d12.src = `images/start/d12/.png`
+}
+}
+
+function twentyGrab(){
+let num1 = getRandomNumber(20)
+twenties.push(num1)
+
+switch (num1){
+  case 1:
+    dTwenty.src = `images/numbers/1.png`
+      break;
+  case 2:
+    dTwenty.src = `images/numbers/2.png`
+      break;
+  case 3:
+    dTwenty.src = `images/numbers/3.png`
+      break;
+  case 4:
+    dTwenty.src = `images/numbers/4.png`
+      break;
+  case 5:
+    dTwenty.src = `images/numbers/5.png` 
+      break
+  case 6:
+    dTwenty.src = `images/numbers/6.png`
+      break;
+  case 7:
+    dTwenty.src = `images/numbers/7.png`
+      break;
+  case 8:
+    dTwenty.src = `images/numbers/8.png`
+      break;
+  case 9:
+    dTwenty.src = `images/numbers/9.png`
+      break;
+  case 10:
+    dTwenty.src = `images/numbers/10.png`
+      break;
+  case 11:
+    dTwenty.src = `images/numbers/11.png`
+      break;
+  case 12:
+    dTwenty.src = `images/numbers/12.png`
+      break;
+  case 13:
+    dTwenty.src = `images/numbers/13.png`
+      break;
+  case 14:
+    dTwenty.src = `images/numbers/14.png`
+      break;
+  case 15:
+    dTwenty.src = `images/numbers/15.png`
+      break;
+  case 16:
+    dTwenty.src = `images/numbers/16.png`
+      break;
+  case 17:
+    dTwenty.src = `images/numbers/17.png`
+      break;
+  case 18:
+    dTwenty.src = `images/numbers/18/png`
+      break;
+  case 19:
+    dTwenty.src = `images/numbers/19.png`
+      break;
+  case 20:
+    dTwenty.src =  `images/numbers/20.png`
+      break;
+  //default:
+    //dTwenty.src = `images/start/d20.jpg`
+}
+}
 
 /*******************
  * EVENT LISTENERS *
  *******************/
-
+dSix.addEventListener("click", sixGrab )
+dSix1.addEventListener('click', doubleSixGrab)
+dSix2.addEventListener('click', doubleSixGrab)
+d12.addEventListener('click', twelveGrab)
+dTwenty.addEventListener('click', twentyGrab)
 
 /****************
  * MATH SECTION *
